6a0663ad1d8a999adaefd2bc3e43e2f0ad4ec555
James Lu <1912525497@qq.com>
2023-08-27 07:01:08 +0800

    优化已知问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -13,29 +13,57 @@ export default createRouter({
         },
         {
           path: '/g6',
-          component: () => import('@/views/G6/G6.vue')
+          component: () => import('@/views/G6/G6.vue'),
+          meta: {
+            title: '小鹏G6 超智驾轿跑SUV'
+          }
         },
         {
           path: '/p7i',
-          component: () => import('@/views/P7i/P7i.vue')
+          component: () => import('@/views/P7i/P7i.vue'),
+          meta: {
+            title: '小鹏全新P7i售价、价格、续航、参数配置'
+          }
         },
         {
           path: '/g9',
-          component: () => import('@/views/G9/G9.vue')
+          component: () => import('@/views/G9/G9.vue'),
+          meta: {
+            title: '小鹏G9售价、价格、续航、参数配置'
+          }
         },
         {
           path: '/p7',
-          component: () => import('@/views/P7/P7.vue')
+          component: () => import('@/views/P7/P7.vue'),
+          meta: {
+            title: '小鹏G9售价、价格、续航、参数配置'
+          }
         },
         {
           path: '/p5',
-          component: () => import('@/views/P5/P5.vue')
+          component: () => import('@/views/P5/P5.vue'),
+          meta: {
+            title: '小鹏G9售价、价格、续航、参数配置'
+          }
         },
         {
           path: '/g3i',
-          component: () => import('@/views/G3i/G3i.vue')
+          component: () => import('@/views/G3i/G3i.vue'),
+          meta: {
+            title: '小鹏G9售价、价格、续航、参数配置'
+          }
         }
       ]
     }
   ]
 })
+
+router.beforeEach((to) => {
+  if (to.meta?.title) {
+    document.title = [to.meta.title, '小鹏汽车官网'].join(' | ')
+  } else {
+    document.title = '小鹏汽车丨未来出行探索者'
+  }
+})
+
+export default router
